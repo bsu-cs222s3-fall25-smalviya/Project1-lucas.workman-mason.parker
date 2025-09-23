2bdbe1bc4ab371e0a53d0b5ae136537b19aa17b4
LucasAWorkman <lucas.workman@bsu.edu>
2025-09-23 16:18:08 -0400

    add more test cases

--- src/test/java/Internal/TestFile.java
+++ src/test/java/Internal/TestFile.java
@@ -1,26 +1,58 @@
 package Internal;
 
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Scanner;
 
 public class TestFile {
 
-    private static final Scanner input = new Scanner(System.in);
     @Test
-    public void ScannerTestCase() throws FetchWikipedia.NoSuchURLException, FetchWikipedia.CouldNotConvertToStringException, FetchWikipedia.BadConnectionException {
-        System.out.print("Enter Wikipedia Name: ");
-        String subject = input.nextLine();
-        FetchWikipedia wikipediaFetcher = new FetchWikipedia(subject); // add scanner testcase
-        System.out.println(wikipediaFetcher.getConversion().getData().title);
+    public void HardcodeTitleShouldMatch() throws FetchWikipedia.NoSuchURLException,
+            FetchWikipedia.CouldNotConvertToStringException,
+            FetchWikipedia.BadConnectionException {
+        FetchWikipedia fetcher = new FetchWikipedia("Albert Einstein");
+        String title = fetcher.getConversion().getData().title;
+        Assertions.assertEquals("Albert Einstein", title);
     }
 
     @Test
-    public void HardcodeTestCase() throws FetchWikipedia.NoSuchURLException, FetchWikipedia.CouldNotConvertToStringException, FetchWikipedia.BadConnectionException {
-        FetchWikipedia wikiepediaFetcher = new FetchWikipedia("George Washington");
-        System.out.println(wikiepediaFetcher.getConversion().getData().id);
+    public void HardcodePageIdShouldNotBeZero() throws FetchWikipedia.NoSuchURLException,
+            FetchWikipedia.CouldNotConvertToStringException,
+            FetchWikipedia.BadConnectionException {
+        FetchWikipedia fetcher = new FetchWikipedia("Isaac Newton");
+        int pageId = fetcher.getConversion().getData().id;
+        Assertions.assertTrue(pageId > 0, "Page id should be greater than zero");
+    }
+
+    @Test
+    public void NoSuchPageShouldThrow() {
+        Assertions.assertThrows(FetchWikipedia.NoSuchURLException.class, () -> {
+            new FetchWikipedia("ThisPageShouldNotExist12345!!");
+        });
+    }
+
+    @Test
+    public void JSONConvertShouldParseValidData() throws Exception {
+        // this should be able to test if json conversion works
+        String mockJson = """
+        {
+          "query": {
+            "pages": {
+              "67420": {
+                "pageid": 67,
+                "title": "Test Subject",
+                "revisions": [
+                  {"user": "Tester", "timestamp": "2025-09-23T00:00:00Z"}
+                ]
+              }
+            }
+          }
+        }
+        """;
+
+        JSONConvert converter = new JSONConvert(mockJson);
+        Assertions.assertEquals(12345, converter.getData().id);
+        Assertions.assertEquals("Test Subject", converter.getData().title);
+        Assertions.assertEquals("Tester", converter.getData().revisions.getFirst().user);
     }
 }
+
