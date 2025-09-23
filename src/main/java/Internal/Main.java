b536aac20a14ed1ec85043867ddaa645b79df4ef
Masonwp123 <MasonParker868@gmail.com>
2025-09-23 14:29:22 -0400

    User can now query for any amount of pages as they want

--- src/main/java/Internal/Main.java
+++ src/main/java/Internal/Main.java
@@ -7,18 +7,52 @@ public class Main {
     private static final Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        JSONConvert jsonConvert = getJSONData();
-        System.out.println(jsonConvert.data.id);
-        System.out.println(jsonConvert.data.title);
+
+        while (getPageAction() == 1) {
+            JSONConvert jsonConvert = getJSONData();
+
+            boolean inPage = true;
+            while (inPage) {
+
+                int action = getAction();
+
+                switch (action) {
+                    case 0: // Back
+                        inPage = false;
+                        break;
+                    case 1: { // Get page data
+                        System.out.println();
+                        System.out.print("Page Title: ");
+                        System.out.println(jsonConvert.data.title);
+                        System.out.print("Page Id: ");
+                        System.out.println(jsonConvert.data.id);
+                        System.out.println();
+                        break;
+                    }
+                    case 2: { // Search for last revision
+                        JSONConvert.Revision revision = jsonConvert.data.revisions.getFirst();
+
+                        System.out.println();
+                        System.out.print("Last edit by: ");
+                        System.out.print(revision.user);
+                        System.out.print(" at ");
+                        System.out.println(revision.timestamp);
+                        System.out.println();
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }
+        }
     }
 
-    private static int getAction() {
-        int action = 0;
-        while (action == 0) {
-            System.out.println("Enter Option: ");
-            System.out.println("(1) Search for last revision ");
-            System.out.println("(2)");
-            System.out.println("(3)");
+    private static int getPageAction() {
+        int action = -1;
+        while (action == -1) {
+            System.out.println("(0) Quit ");
+            System.out.println("(1) Search for page ");
+            System.out.print("Enter Option: ");
             String sOption = input.nextLine();
 
             try {
@@ -27,15 +61,41 @@ public class Main {
                 System.out.println("Invalid Option, please try again.");
                 continue;
             }
-            if (action < 1 || action > 3) {
+            if (action < 0 || action > 1) {
                 System.out.println("Invalid Option, please try again.");
-                action = 0;
+                action = -1;
+            }
+        }
+        return action;
+    }
+
+    private static int getAction() {
+        int action = -1;
+        while (action == -1) {
+            System.out.println("(0) Back ");
+            System.out.println("(1) Get page data ");
+            System.out.println("(2) Search for last revision ");
+            System.out.print("Enter Option: ");
+            String sOption = input.nextLine();
+
+            try {
+                action = Integer.parseInt(sOption);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Option, please try again.");
+                continue;
+            }
+            if (action < 0 || action > 2) {
+                System.out.println("Invalid Option, please try again.");
+                action = -1;
             }
         }
         return action;
     }
 
     private static JSONConvert getJSONData() {
+
+
+
         System.out.print("Enter Wikipedia Name: ");
         String subject = input.nextLine();
         FetchWikipedia wikipediaFetch;
