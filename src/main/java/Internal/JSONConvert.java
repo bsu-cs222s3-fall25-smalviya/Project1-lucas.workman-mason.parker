96d52702670380ef5fcce38d7c66dd8eaabf30fc
Masonwp123 <MasonParker868@gmail.com>
2025-09-23 13:43:22 -0400

    - GSON now works for setting JSONConvert Data

--- src/main/java/Internal/JSONConvert.java
+++ src/main/java/Internal/JSONConvert.java
@@ -1,18 +1,52 @@
 package Internal;
 
-import java.lang.reflect.Type;
-
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.*;
 
 public class JSONConvert {
 
-    public String data;
+    public Data data;
 
-    private final Gson gson = new Gson();
+    public JSONConvert(String jsonData) {
+        this.data = new Data();
 
-    public String getGson(String jsonData) {
-        this.data = jsonData;
-        return String.valueOf(gson.fromJson(this.data, (Type) String.class));
+
+
+        Gson gson = new Gson();
+
+        JsonElement jsonElement = new JsonParser().parse(jsonData);
+        JsonElement pages = jsonElement.getAsJsonObject().get("query").getAsJsonObject().get("pages");
+
+        Set<Map.Entry<String, JsonElement>> entrySet = pages.getAsJsonObject().entrySet();
+
+        JsonElement yourDesiredElement = null;
+
+        for(Map.Entry<String,JsonElement> entry : entrySet){
+            yourDesiredElement = entry.getValue();
+        }
+
+        this.data = gson.fromJson(yourDesiredElement.toString(), Data.class);
+
+        //TODO: convert to useful data and add getters
+
+    }
+
+    static class Revision {
+        String user;
+        String anon;
+        String timestamp;
+    }
+
+    static class Data {
+        @SerializedName("pageid")
+        int id;
+        int ns;
+        String title;
+        ArrayList<Revision> revisions;
     }
 
 }
--- src/main/java/Internal/Main.java
+++ src/main/java/Internal/Main.java
@@ -8,6 +8,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         FetchWikipedia wikipediaFetch = new FetchWikipedia();
-        System.out.println(wikipediaFetch.getConversion().data);
+        System.out.println(wikipediaFetch.getConversion().data.id);
     }
 }
