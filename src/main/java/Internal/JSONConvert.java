bfca29de9043232ac0d5757523ad09c299de0b67
Masonwp123 <MasonParker868@gmail.com>
2025-09-23 12:45:30 -0400

    - added data for test purposes

--- src/main/java/Internal/JSONConvert.java
+++ src/main/java/Internal/JSONConvert.java
@@ -6,8 +6,12 @@ import java.nio.charset.Charset;
 
 public class JSONConvert {
 
+    public String data;
+
     public JSONConvert(String jsonData) {
+        this.data = jsonData;
         //TODO: convert to useful data and add getters
+
     }
 
 }
