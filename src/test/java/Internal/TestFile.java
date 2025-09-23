9ea757c682a3f9d51cfbe9295edfe5109dfbc2fc
LucasAWorkman <lucas.workman@bsu.edu>
2025-09-23 15:08:11 -0400

    update testcase to new class structure

--- src/test/java/Internal/TestFile.java
+++ src/test/java/Internal/TestFile.java
@@ -5,13 +5,22 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Scanner;
 
 public class TestFile {
 
+    private static final Scanner input = new Scanner(System.in);
+    @Test
+    public void ScannerTestCase() throws FetchWikipedia.NoSuchURLException, FetchWikipedia.CouldNotConvertToStringException, FetchWikipedia.BadConnectionException {
+        System.out.print("Enter Wikipedia Name: ");
+        String subject = input.nextLine();
+        FetchWikipedia wikipediaFetcher = new FetchWikipedia(subject); // add scanner testcase
+        System.out.println(wikipediaFetcher.getConversion().getData().title);
+    }
 
     @Test
-    public void File() throws FetchWikipedia.NoSuchURLException, FetchWikipedia.CouldNotConvertToStringException, FetchWikipedia.BadConnectionException {
-        FetchWikipedia wikipediaFetcher = new FetchWikipedia("George Washington");
-
+    public void HardcodeTestCase() throws FetchWikipedia.NoSuchURLException, FetchWikipedia.CouldNotConvertToStringException, FetchWikipedia.BadConnectionException {
+        FetchWikipedia wikiepediaFetcher = new FetchWikipedia("George Washington");
+        System.out.println(wikiepediaFetcher.getConversion().getData().id);
     }
 }
