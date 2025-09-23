f1d7f5f5169c7b677b603f0b1ffceb88958f433b
Masonwp123 <MasonParker868@gmail.com>
2025-09-23 14:17:31 -0400

    Added an action which could do multiple things

--- src/main/java/Internal/Main.java
+++ src/main/java/Internal/Main.java
@@ -4,14 +4,38 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static final Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
         JSONConvert jsonConvert = getJSONData();
         System.out.println(jsonConvert.data.id);
         System.out.println(jsonConvert.data.title);
     }
 
+    private static int getAction() {
+        int action = 0;
+        while (action == 0) {
+            System.out.println("Enter Option: ");
+            System.out.println("(1) Search for last revision ");
+            System.out.println("(2)");
+            System.out.println("(3)");
+            String sOption = input.nextLine();
+
+            try {
+                action = Integer.parseInt(sOption);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Option, please try again.");
+                continue;
+            }
+            if (action < 1 || action > 3) {
+                System.out.println("Invalid Option, please try again.");
+                action = 0;
+            }
+        }
+        return action;
+    }
+
     private static JSONConvert getJSONData() {
-        Scanner input = new Scanner(System.in);
         System.out.print("Enter Wikipedia Name: ");
         String subject = input.nextLine();
         FetchWikipedia wikipediaFetch;
